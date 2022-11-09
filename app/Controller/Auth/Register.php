97930c6c187c109dde03326fb07e60c19177c163
neoan <stefan.roehrl@gmail.com>
2022-11-09 10:57:46 -0500

    version update

--- app/Controller/Auth/Register.php
+++ app/Controller/Auth/Register.php
@@ -5,7 +5,6 @@ namespace App\Controller\Auth;
 use App\Model\User;
 use Neoan\Errors\Unauthorized;
 use Neoan\Request\Request;
-use Neoan\Response\Response;
 use Neoan\Routing\Interfaces\Routable;
 
 class Register implements Routable
